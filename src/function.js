3bb156b33b7c3b5785c4ae10f106f68b4c6ef44a
Eric Hacke <eric@roleup.io>
2020-07-07 21:23:04 -0400

    with load

--- src/function.js
+++ src/function.js
@@ -5,7 +5,7 @@ exports.subprocess = (req, res) => {
 
   console.log('Before mocha');
   // eslint-disable-next-line no-process-env
-  execSync('sleep 0.5', { stdio: 'inherit', env: { PATH: process.env.PATH, DEBUG: 'mocha*' } });
+  execSync('./node_modules/nodehog/bin.js cpu 500 1 1', { stdio: 'inherit', env: { PATH: process.env.PATH } });
 
   const duration = Date.now() - start;
   console.log(`Execution: ${duration} ms`);
