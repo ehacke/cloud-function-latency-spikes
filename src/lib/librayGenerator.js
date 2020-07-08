0192a1b48e4161979b8ec1850242bc53775b05ff
Eric Hacke <eric@roleup.io>
2020-07-08 16:01:50 -0400

    updates

--- src/lib/librayGenerator.js
+++ src/lib/librayGenerator.js
@@ -4,7 +4,7 @@ const { times } = require('lodash');
 const reserved = require('reserved-words');
 const fs = require('fs-extra');
 
-const MAX_FILES = 1500;
+const MAX_FILES = 500;
 let counter = 0;
 
 /**
--- src/stats/localResults.js
+++ src/stats/localResults.js
@@ -4,9 +4,9 @@ const { start } = require('./invokeAndRecord');
 (async () => {
   const OUTPUT_PATH = path.join(__dirname, '../local');
 
-  await start('http://localhost:3000', 'load', OUTPUT_PATH, 1000);
-  await start('http://localhost:3000', 'hog', OUTPUT_PATH, 1500);
-  await start('http://localhost:3000', 'regenerate', OUTPUT_PATH, 5000);
+  await start('http://localhost:3000', 'load', OUTPUT_PATH, 3000);
+  await start('http://localhost:3000', 'hog', OUTPUT_PATH, 2000);
+  await start('http://localhost:3000', 'regenerate', OUTPUT_PATH, 6000);
 
   // eslint-disable-next-line no-process-exit,unicorn/no-process-exit
   process.exit(0);
