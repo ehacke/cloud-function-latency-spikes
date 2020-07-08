e17885e088e49d3c87f6ac59bc145f67e736b6c7
Eric Hacke <eric@roleup.io>
2020-07-08 08:54:49 -0400

    stopping for now

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # cloud-run-vs-cloud-functions-processes
 How child processes are handled in Cloud Run compared to Cloud Functions
+
+## Status
+
+At this point I can reproduce the issue with Cloud Functions sporadically running slower. Seems to be most obvious 
+when running Mocha inside a thread on a 512MB function. But can't figure out why.
+
+Seems likely that it's happening between the moment that the Cloud Function first invokes Mocha, and after all the 
+first batch of requires fire.
+
+Maybe some kind of system-level issue with reading from disk? I dunno. 
--- src/function.js
+++ src/function.js
@@ -5,7 +5,8 @@ exports.subprocess = (req, res) => {
 
   console.log('Before mocha');
   // eslint-disable-next-line no-process-env
-  execSync('./node_modules/mocha/bin/mocha **/*.unit.js', { stdio: 'inherit', env: { PATH: process.env.PATH } });
+  execSync('echo "foo"; ./node_modules/mocha/bin/mocha **/*.unit.js', { stdio: 'inherit', env: { PATH: process.env.PATH, DEBUG: 'mocha*' } });
+  // execSync('./node_modules/nodehog/bin.js cpu 1000 1 1', { stdio: 'inherit', env: { PATH: process.env.PATH } });
 
   const duration = Date.now() - start;
   console.log(`Execution: ${duration} ms`);
