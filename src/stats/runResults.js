56aa788c62e31ad808268e24be67ff9d76cc1bd6
Eric Hacke <eric@roleup.io>
2020-07-08 20:11:38 -0400

    minor update

--- src/stats/runResults.js
+++ src/stats/runResults.js
@@ -6,9 +6,9 @@ require('dotenv').config();
 (async () => {
   const OUTPUT_PATH = path.join(__dirname, '../run');
 
-  await start(getenv('CLOUD_RUN_URL'), 'load', OUTPUT_PATH, 2000);
+  await start(getenv('CLOUD_RUN_URL'), 'load', OUTPUT_PATH, 4000);
   await start(getenv('CLOUD_RUN_URL'), 'hog', OUTPUT_PATH, 2000);
-  await start(getenv('CLOUD_RUN_URL'), 'regenerate', OUTPUT_PATH, 5000);
+  await start(getenv('CLOUD_RUN_URL'), 'regenerate', OUTPUT_PATH, 6000);
 
   // eslint-disable-next-line no-process-exit,unicorn/no-process-exit
   process.exit(0);
